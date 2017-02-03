ae8b5593b5e4a9ddb67d66d048484b8c0db4ac01
Nyx <a.f.salangoste@gmail.com>
2017-02-03 11:21:11 +0800

    Modified TCCommands2

--- TCCommands2.cob
+++ TCCommands2.cob
@@ -9,8 +9,20 @@
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
+       01 Students.
+           02 Males    PIC 9(3)    VALUE 611.
+           02 Females  PIC 9(4)    VALUE 1225.
+           02 TotalStudents    PIC 9999.
+
+       01 Money.
+           02 Cash     PIC 9(3) VALUE 123.
+           02 Cheque   PIC 9(4) VALUE 1533.
+           02 Total    PIC 9(4) VALUE 1000.
+
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
-            DISPLAY "Hello world"
-            STOP RUN.
+           ADD Cash,20 TO Total.
+           DISPLAY Total.
+
+           STOP RUN.
        END PROGRAM TCCommands2.
