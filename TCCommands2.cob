4dbc4665a1102e08ae4c6132ac94e4f09c9240fe
Nyx <a.f.salangoste@gmail.com>
2017-02-03 11:01:09 +0800

    Added TCCommands2.cob

---
+++ TCCommands2.cob
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author:Nyx
+      * Date:2/3/2017
+      * Purpose:Learning Cobol Programming
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. TCCommands2.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            STOP RUN.
+       END PROGRAM TCCommands2.
