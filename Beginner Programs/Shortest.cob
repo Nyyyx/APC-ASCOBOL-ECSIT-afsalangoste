a5c0b06e92a052fce27f9bb9e8c297b1ffc6960e
Nyx <a.f.salangoste@gmail.com>
2017-02-26 16:36:36 +0800

    Added 3 folder: Beginner Programs, Selection and Iteration, and Sequential Files.

---
+++ Beginner Programs/Shortest.cob
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author: Nyx
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Shortest.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "I did it!!"
+            STOP RUN.
+       END PROGRAM Shortest.
